ea30ac46998e92d665e3e6dc3ea44156ec3ed9c6
Evgeny Kulpinov <evgen2309@yandex.ru>
2024-04-02 02:23:33 +0300

    v3

--- src/main/java/overridetech/jdbc/jpa/service/UserServiceImpl.java
+++ src/main/java/overridetech/jdbc/jpa/service/UserServiceImpl.java
@@ -1,52 +1,51 @@
 package overridetech.jdbc.jpa.service;
 
-import org.hibernate.SessionFactory;
 import overridetech.jdbc.jpa.dao.UserDaoHibernateImpl;
+import overridetech.jdbc.jpa.dao.UserDaoJDBCImpl;
 import overridetech.jdbc.jpa.model.User;
 import overridetech.jdbc.jpa.util.Util;
 
-import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
     Util util = new Util();
 
-    private final Connection connection;
-    private final SessionFactory sessionFactory;
+    private final UserDaoJDBCImpl userDaoJDBC;
+    private final UserDaoHibernateImpl userDaoHibernate;
 
     public UserServiceImpl() {
-        this.connection = Util.getPostgreSQLConnection();
-        this.sessionFactory = util.getSessionFactory();
+        this.userDaoJDBC = new UserDaoJDBCImpl(Util.getPostgreSQLConnection());
+        this.userDaoHibernate = new UserDaoHibernateImpl(util.getSessionFactory());
     }
 
     public void createUsersTable() throws SQLException {
-//        new UserDaoJDBCImpl(connection).createUsersTable();
-        new UserDaoHibernateImpl(sessionFactory).createUsersTable();
+//        userDaoJDBC.createUsersTable();
+        userDaoHibernate.createUsersTable();
     }
 
     public void dropUsersTable() throws SQLException {
-//        new UserDaoJDBCImpl(connection).dropUsersTable();
-        new UserDaoHibernateImpl(sessionFactory).dropUsersTable();
+//        userDaoJDBC.dropUsersTable();
+        userDaoHibernate.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) throws SQLException {
-//        new UserDaoJDBCImpl(connection).saveUser(name, lastName, age);
-        new UserDaoHibernateImpl(sessionFactory).saveUser(name, lastName, age);
+//        userDaoJDBC.saveUser(name, lastName, age);
+        userDaoHibernate.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) throws SQLException {
-//        new UserDaoJDBCImpl(connection).removeUserById(id);
-        new UserDaoHibernateImpl(sessionFactory).removeUserById(id);
+//        userDaoJDBC.removeUserById(id);
+        userDaoHibernate.removeUserById(id);
     }
 
     public List<User> getAllUsers() throws SQLException {
-//        return new UserDaoJDBCImpl(connection).getAllUsers();
-        return new UserDaoHibernateImpl(sessionFactory).getAllUsers();
+//        userDaoJDBC.getAllUsers();
+        return userDaoHibernate.getAllUsers();
     }
 
     public void cleanUsersTable() throws SQLException {
-//        new UserDaoJDBCImpl(connection).cleanUsersTable();
-        new UserDaoHibernateImpl(sessionFactory).cleanUsersTable();
+//        userDaoJDBC.cleanUsersTable();
+        userDaoHibernate.cleanUsersTable();
     }
 }
